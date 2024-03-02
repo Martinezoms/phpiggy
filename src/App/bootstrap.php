19ccd8861e6358d64ae45a677db0ac5f903eb421
Martins Enyinnaya <martinezoms15@gmail.com>
2024-03-02 21:11:40 +0100

    Update: Router and routing implementations

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
+
+//Routes
+$app->get('/', [HomeController::class, 'home']);
+$app->get('/logout', [HomeController::class, 'home']);
 
 return $app;
